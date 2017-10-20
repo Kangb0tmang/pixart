d60dd5bd0938ce9b4163b2ff66c582454eba0999
Jon Kang <Jon.Kang.JD@gmail.com>
2017-10-20 14:54:06 +1100

    set color on enter key

--- pixart.js
+++ pixart.js
@@ -4,8 +4,19 @@ var $brush = $('.brush');
 var $color_field = $('#color-field');
 var $set_color = $('#set-color');
 
-$set_color.on('click', function(e)
+// $set_color.on('click', function(e)
+// {
+//   e.preventDefault();
+//   $brush.css("background", $color_field.val());
+// });
+
+// Set Color on 'Enter key'
+$color_field.keypress(function(e)
 {
-  e.preventDefault();
-  $brush.css("background", $color_field.val());
+  var key = e.which;
+  if (key == 13)
+  {
+    $brush.css("background", $color_field.val()).click();
+    return false;
+  }
 });
